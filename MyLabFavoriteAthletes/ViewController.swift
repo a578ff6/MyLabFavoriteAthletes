4d12b50cf1162c5bb0b8a686332fba59beb7e834
wayne tsao <a578ff6@gmail.com>
2023-10-05 20:27:22 +0800

    Initial Commit

---
+++ MyLabFavoriteAthletes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLabFavoriteAthletes
+//
+//  Created by 曹家瑋 on 2023/10/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
